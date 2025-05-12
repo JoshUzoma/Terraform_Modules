72f117d4a91c3e41ed9fc9ed597ac6aeb518cda6
JoshUzoma <josh.uzoma@steampunk.com>
2025-05-12 14:08:17 -0500

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "sp-dccc-tfstate-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "sp-dccc-tfstate-locks"
-    encrypt        = true
-  }
-}
+
